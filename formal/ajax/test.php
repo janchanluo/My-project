ee6c049cb0c9a29cd1651f89cfcd12619c0d532a
janchanluo <614512632@qq.com>
2018-08-13 17:50:34 +0800

    其他

---
+++ formal/ajax/test.php
@@ -0,0 +1,10 @@
+<?php
+$_POST['name']='xiaohong';
+$_POST['age']=14;
+ $msg='Hello,'.$_POST['name'].',your age is '.$_POST['age'].'!';
+ $a='select * from userinfo where username="xiaohong"';
+ echo $msg;
+$_POST['username']="xxx"
+
+
+?>
